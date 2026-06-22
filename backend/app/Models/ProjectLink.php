d552869583133e26e4a778e4a20ce560807006ad
NGP Dev <dev@ngptechworld.com>
2026-06-22 23:58:59 +0300

    frontend

--- backend/app/Models/ProjectLink.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class ProjectLink extends Model
-{
-    protected $guarded = [];
-    public function project() { return $this->belongsTo(Project::class); }
-}
